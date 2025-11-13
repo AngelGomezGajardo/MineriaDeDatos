a443c191107b7d991511b53e429c0192d1530e0b
AngelGomezMirs <angel.gomez@mirsrobotics.com>
2025-11-13 18:42:29 -0300

    update 2

---
+++ Bearing Dataset/matlab2df2csv.py
@@ -0,0 +1,38 @@
+#install pip install scipy
+import pandas as pd
+from scipy.io import loadmat
+import numpy as np
+
+data=loadmat("100.mat")
+
+#revisamos que hay en el archivo,
+#print(data.keys())
+#print(data.values())
+
+#con shape mostramos la forma
+#en general cuando hacemos .algo <- sin parentesis, es para mostrar tu forma, un atributo
+# cuando hacemos .algo() <- estamos llamando a una funcion que actua sobre lo que esta antes del punto.
+print(data["X100_DE_time"].shape)     # OK
+print(data["X100_FE_time"].shape)     # OK
+print(data["X100RPM"].shape)
+
+#convertimos ahora a flat para trabajar con dataframes
+# al ser archivos que vienen de matlab, necesitamos nosotros generar la tabla de datos con columnas y filas
+# por eso es que se convierte a flatten y despues armamos el datafame, #pandas no tiene lectura nativa de matlab
+DE = data["X100_DE_time"].flatten()
+FE = data["X100_FE_time"].flatten()
+RPM = data["X100RPM"].flatten()
+print(RPM)
+#conocemos el largo de DE
+n=len(DE)
+
+#generamos el dataframe df
+df = pd.DataFrame({
+    "sample": np.arange(n),
+    "vib_DE": DE,
+    "vib_FE": FE,
+    "RPM"   : RPM[0],
+})
+print(df)
+df.to_csv('Data.csv')
+
